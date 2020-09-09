cfaf641718ca29a5b36deecc2df9ada2202e7c36
RLBurst <70789237+RLBurst@users.noreply.github.com>
2020-09-08 20:58:06 -0400

    Updating about
    Remove div container for now.

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,56 +16,53 @@ const About = ({ data }) => {
       <Helmet>
         <title>{`About | ${data.site.siteMetadata.title}`}</title>
       </Helmet>
-      <div
+      <Img
+        fixed={avatar.childImageSharp.fixed}
         sx={{
-          mt: '3rem',
-          mr: '2rem',
+          mt: '2rem',
+          mr: '1rem',
           float: 'left',
           width: `${avatar.width}`,
         }}
-      >
-        <Img fixed={avatar.childImageSharp.fixed} />
-      </div>
-      <div sx={{ mt: '2rem' }}>
-        <article>
-          <section>
-            <p>
-              Hi! I'm Ron, <strike>a software developer.</strike>
-            </p>
-            <p>
-              This is a minimal styled Gatsby site that I created in my spare
-              time. I have always liked simple designs and I do not consider
-              myself a full stack developer so creating something simple was
-              also a nice introduction.
-            </p>
-            <p>
-              I wanted a portfolio like website as an alternative to providing a
-              bunch of links to GitHub repositories or GitHub pages and a place
-              where I could just write my thoughts away. I am a huge fan of
-              writing!
-            </p>
-            <p>
-              Some of the features of this site so far include a blog and
-              projects showcase, light and dark mode support, and continuous
-              deployment with Netlify.
-            </p>
-            <p>
-              There are still a lot of features and tweaks *cough* bugs *cough*
-              that can be done to make the site better. The good news is that
-              there's plugins for almost everything! So it may be easy...but for
-              now I am more focused on content such as writing and other coding
-              projects. However, I will create a todo list for this site in the
-              future.
-            </p>
-            <p>
-              That's just a placeholder picture for now. Found it on Reddit so
-              all credits to u/Myunin. Really liked it because it was a Sylveon
-              and Umbreon color pallete which fits in to the whole light and
-              dark mode (shush I know technically it should be Espeon!).
-            </p>
-          </section>
-        </article>
-      </div>
+      />
+      <article>
+        <section>
+          <p>
+            Hi! I'm Ron, <strike>a software developer.</strike>
+          </p>
+          <p>
+            This is a minimal styled Gatsby site that I created in my spare
+            time. I have always liked simple designs and I do not consider
+            myself a full stack developer so creating something simple was also
+            a nice introduction.
+          </p>
+          <p>
+            I wanted a portfolio like website as an alternative to providing a
+            bunch of links to GitHub repositories or GitHub pages and a place
+            where I could just write my thoughts away. I am a huge fan of
+            writing!
+          </p>
+          <p>
+            Some of the features of this site so far include a blog and projects
+            showcase, light and dark mode support, and continuous deployment
+            with Netlify.
+          </p>
+          <p>
+            There are still a lot of features and tweaks *cough* bugs *cough*
+            that can be done to make the site better. The good news is that
+            there's plugins for almost everything! So it may be easy...but for
+            now I am more focused on content such as writing and other coding
+            projects. However, I will create a todo list for this site in the
+            future.
+          </p>
+          <p>
+            That's just a placeholder picture for now. Found it on Reddit so all
+            credits to u/Myunin. Really liked it because it was a Sylveon and
+            Umbreon color pallete which fits in to the whole light and dark mode
+            (shush I know technically it should be Espeon!).
+          </p>
+        </section>
+      </article>
     </Layout>
   )
 }
